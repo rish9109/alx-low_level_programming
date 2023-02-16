9bcb74f868e2ee15c30abc902490d81b7e44d093
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-02-17 00:26:49 +0200

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
+
 /**
-* main - A program that prints a line using the printf function
-* Return: 0 (sucess)
+* main - prints "with proper grammar, but the outcome is a piece of art,
+* Return: Always 0.
+*
 */
+
 int main(void)
-[
+  
 printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 }
-
