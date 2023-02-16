2939724e7d839bd135d25bcd3867f62e13664dff
rish9109 <ramkis007@gmail.com>
2023-02-16 13:38:05 -0800

    2nd part

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include  <stdio.h>
+/**
+ ** main - prints "Programming is like building a multilingual puzzle.
+ * Return: Always 0 (success)
+ */;	
+
+int main(void)
+{
+puts("\"Progiamming is like building a multilingual puzzle");	
+return (0);
+}
