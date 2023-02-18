78bb23a5e650ecf59be6b2ca351833f164a2613e
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-02-18 16:47:54 +0200

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 
 /* comments here*/
 
-int main(void) 
+int main(void)
 {
-    puts ("\"Programming is like building a multilingual puzzle");
-    return 0;
+    puts("\"Programming is like building a multilingual puzzle");
+    return (0);
 }
