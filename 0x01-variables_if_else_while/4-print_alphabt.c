0442ed7c349918858280d4964a6f938b7d876681
rish9109 <ramkis007@gmail.com>
2023-02-18 13:43:11 -0800

    3n4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main prints letters except q and e 
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	char ch = 'a';
+	while (ch <= 'z')
+	{
+		if ((ch != 'e') && (ch != 'q'))
+		{
+			putchar(ch);
+		}
+		ch++;
+	}
+	putchar9('\n');
+	return (0)
+}
