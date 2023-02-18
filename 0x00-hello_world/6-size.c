e31501672ee73aad26524b6515fa889c959beff1
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-02-18 18:51:43 +0200

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,7 +12,7 @@ printf("The size of int is %zu bytes.\n", sizeof(int));
 printf("The size of long int is %zu bytes.\n", sizeof(long));
 printf("The size of long long int is %zu bytes.\n", sizeof(long long));
 printf("The size of float is %zu bytes.\n", sizeof(float));
-return (0);
+printf("\n");
   
 printf("On a 64-bit platform:\n")
 printf("The size of char is %zu bytes.\n", sizeof(char));
@@ -20,5 +20,7 @@ printf("The size of int is %zu bytes.\n", sizeof(int));
 printf("The size of long int is %zu bytes.\n", sizeof(long));
 printf("The size of long long int is %zu bytes.\n", sizeof(long long));
 printf("The size of float is %zu bytes.\n", sizeof(float));
+printf("\n");
+  
 return (0);
 }
