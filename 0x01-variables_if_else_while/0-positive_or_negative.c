8fd3bf2e874b7fba990b320218245befea56bcb4
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-02-18 22:34:20 +0200

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/* main - to generate random number*/
+
+/** main - to generate random number
+*
+* Return: Always 0
+*
+*/
 {
 	int n;
 
