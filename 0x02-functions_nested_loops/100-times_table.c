4dee43cd9f9dea7d6a1044e7733e9b0e30347461
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-02-22 23:25:00 +0200

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,9 +2,9 @@
 /**
  * print_times_table - Entry point
  * @n: input
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
-void print_times_table(int n);
+void print_times_table(int n)
 {
-return (0);
+  return (0);
 }
