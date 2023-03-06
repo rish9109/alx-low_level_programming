344f27a8a306b347a5f2dbeabffbc7b193aa7759
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-03-06 22:22:08 +0200

    Update main.h

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef MAIN_H
+#define MAIN_H
 
 char *_memset(char *s, char b, unsigned int n);
 int _putchar(char c);
@@ -12,3 +12,4 @@ void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
 void set_string(char **s, char *to);
 
+#endif
