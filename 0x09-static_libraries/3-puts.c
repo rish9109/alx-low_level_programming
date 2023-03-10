a4ea810d64e53094c0814a7f66d4dd3baba87897
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-03-10 21:00:21 +0200

    Update 3-puts.c

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,12 @@
-#include "main.h"
-void _puts(char *str)
-#include<stdio.h>
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-void _puts(char *str)
-{
-	int a;
+#include <stdio.h>
 
-	for (a = 0; str[a] != '\0'; a++)
-	{
-		_putchar(str[a]);
-	}
-	_putchar('\n');
+/**
+* main - puts "Programming is like building a multilingual puzzle
+* Return: - Always 0.
+*
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
