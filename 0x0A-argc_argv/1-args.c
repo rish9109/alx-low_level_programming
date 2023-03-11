7f0bfb6cbc1740dc44ab42ad0ed1e7cededcf08b
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-03-11 18:13:25 +0200

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -3,11 +3,11 @@
 /**
  * main - print the number of arguments passed to the program
  * @argc: number of arguments
- * @argv: array of arguments	
+ * @argv: array of arguments
  *
  * Return: Always 0 (Success)
  */
-int main(int argc, char *argv[])	
+int main(int argc, char *argv[])
 {
 (void) argv; /*Ignore argv*/
 	printf("%d\n", argc - 1);
