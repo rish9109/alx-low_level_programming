109afc6eb2019fab06685a8e33e29a2f647dc316
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-03-13 20:49:04 +0200

    Update 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -6,19 +6,19 @@
  * @str: char
  * Return: 0
  */
-char *_strdup(char *str)	
+char *_strdup(char *str)
 {
-	char *n;	
-	int i, r = 0;	
-	if (str == NULL)	
-		return (NULL);	
+	char *n;
+	int i, r = 0;
+	if (str == NULL)
+		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
-		i++;	
-	n = malloc(sizeof(char) * (i + 1));	
+		i++;
+	n = malloc(sizeof(char) * (i + 1));
 	if (n == NULL)
 		return (NULL);
 	for (r = 0; str[r]; r++)
 		n[r] = str[r];
-	return (n);	
+	return (n);
 }
