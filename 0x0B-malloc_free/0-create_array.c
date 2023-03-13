3f8a2e1c1a8fcf8499700481eb8480ee27e37068
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-03-13 20:29:33 +0200

    Update 0-create_array.c

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -3,8 +3,8 @@
 /**
  * create_array - create array of size size and assign char c
  * @size: size of array
- * @c: char to assign	
- * Description: creat array of size size and assign char c	
+ * @c: char to assign
+ * Description: creat array of size size and assign char c
  * Return: pointer to array, NULL if fail
  *
  */
