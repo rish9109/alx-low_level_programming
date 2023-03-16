07ef1e0a0db0261e51e21d23a6b5594c384079c9
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-03-16 14:43:31 +0200

    Update 3-array_range.c

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -3,8 +3,7 @@
 /**
  * *array_range - creates an array of integers
  * @min: minimum range of values stored
- * @max: maximum range of values stored and number of elements	
- *
+ * @max: maximum range of values stored and number of elements
  * Return: pointer to the new array
  */
 int *array_range(int min, int max)
