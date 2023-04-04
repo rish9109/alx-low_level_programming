a64f7eafe044c8bdd34c6d9f541b37543cb69ef8
Rish9109 <122869669+rish9109@users.noreply.github.com>
2023-04-04 08:04:42 +0200

    Update 1-listint_len.c

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -8,13 +8,14 @@
  */
 size_t listint_len(const listint_t *h)
 {
- size_t num = 0;
+ unsigned int count = 0;
 
- while (h)
- {
- num++;
- h = h->next;
- }
-
- return (num);
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		count += 1;
+		h = h->next;
+	}
+	return (count);
 }
